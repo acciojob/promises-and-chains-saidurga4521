f404d3785c525c2e6ce1ca14d97feac779b27b17
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-04-24 12:32:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+let AgeE1=document.getElementById("age");
+let nameE1=document.getElementById("name");
+const btnE1=document.getElementById("btn");
+btnE1.addEventListener('click',()=>{
+	if(!AgeE1.value || !nameE1.value){
+		alert("Please enter valid details");
+		
+	}else{
+		function Validate(age){
+		return new Promise((resolve,reject)=>{
+			if(age>18){
+				setTimeout(()=>{
+					resolve("Welcome, . You can vote.");
+				},4000);
+			}else{
+				setTimeout(()=>{
+					reject("Oh sorry . You aren't old enough.");
+				},4000);
+			}
+		})
+	}
+	let age=AgeE1.value;
+	Validate(age).then((res)=>{
+			alert(res);
+	}).catch((err)=>{
+		    alert(err);
+	})
+	}
+	
+})
